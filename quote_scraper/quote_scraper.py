237c774e2eba17079911106bf74f86129d3c36e3
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-08-17 14:51:56 -0400

    Refarctor quote game into two files

--- quote_scraper/quote_scraper.py
+++ quote_scraper/quote_scraper.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from time import sleep
 from random import choice
+from csv import DictWriter
 
 base_url = "http://quotes.toscrape.com"
 
@@ -23,45 +24,17 @@ def scrape_quotes():
 
 		next_btn = soup.find(class_="next")
 		url = next_btn.find("a")["href"] if next_btn else None
-
+		sleep(1)
 	return all_quotes
 
-def start_game(quotes):
-	quote = choice(quotes)
-	remaining_guesses = 4
-	print("Here's a quote")
-	print(quote["text"])
-	print(quote["author"])
-	guess =''
-
-	while guess.lower() != quote["author"].lower() and remaining_guesses > 0:
-		guess = input(f"Who said this quote? \nGuesses remaining: {remaining_guesses}\n")
-		if guess.lower() == quote["author"].lower():
-			print("You got it right!")
-			break
-		remaining_guesses -= 1
-		if remaining_guesses == 3:
-			res = requests.get(f"{base_url}{quote['bio-link']}")
-			soup = BeautifulSoup(res.text, 'html.parser')
-			birth_date = soup.find(class_="author-born-date").get_text()
-			birth_place = soup.find(class_="author-born-location").get_text()
-			print(f"Here's a hint: The author was born {birth_place} on {birth_date}")
-		elif remaining_guesses == 2:
-			print(f"Here's a hint: The author's first name starts with: {quote['author'][0]}")
-		elif remaining_guesses == 1:
-			last_initial = quote["author"].split(" ")[1][0]
-			print(f"Here's a hint: The author's last name starts with: {last_initial}")
-		else:
-			print(f"Sorry, you ran out of guesses. The answer was {quote['author']}")
-
-	again = ''
-	while again not in ('y', 'yes', 'n', 'no'):
-		again = input("Would you like to play again? (y/n)")
-
-	if again.lower() in ('yes', 'y'):
-		return start_game(quotes)
-	else:
-		print("Ok, good bye!")
+# write quotes to a csv file
+def write_quotes(quotes):
+	with open("quotes.csv", "w") as file:
+		headers = ["text", "author", "bio-link"]
+		csv_writer = DictWriter(file, fieldnames=headers)
+		csv_writer.writeheader()
+		for quote in quotes:
+			csv_writer.writerow(quote)
 
 quotes = scrape_quotes()
-start_game(quotes)
+write_quotes(quotes)
